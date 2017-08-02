5ad252283ccf37b8faac90e2bebe04afb667397b
leto <1907984744@qq.com>
2017-08-02 20:58:05 +0800

    Add init.sh, modified README and deploy.sh.Ref #18

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-live video streaming
+# Live Video Boardcast
+
+### To init the project
+After clone the project from git, use command below to init  your project:
+
+```shell
+cd Group2
+. ./init.sh
+```
+Notice that **the first point** cannot be ignored.  
+
+### To run the server
+Then you can use this:
+
+```shell
+. ./deploy.sh
+```
+Similarly you should use a point at first.
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
+echo 'Start venv'
+source ./.venv/bin/activate
+echo 'Done...'
+
+echo 'Make migrations'
+python manage.py makemigrations
+
+echo 'migrate'
+python manage.py migrate
+echo 'Migrate done...'
+
 echo 'Running npm build'
 npm run build
 echo 'Done...'
 
 echo 'Format index.html as Jinja template'
 python format_index_html.py
-echo 'Done...'
-
-echo 'Install python modules'
-pip install -r requirements.txt
 echo 'Done...'
 
 echo 'Collect static'
