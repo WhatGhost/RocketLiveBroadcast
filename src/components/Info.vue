9d4f269698579ff9f0bc2037b7afe74dea20b85f
tsy <779107309@qq.com>
2017-08-04 00:21:55 +0800

    Add personal information pages to project.Ref #23

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,12 +1,11 @@
 <template>
     <div id='title1'>
-        <div id='biaoqian'>
-            <button id='pi' @click="zhuanHuan='PersonalInformation'">个人信息</button>
-            <button id='mp' @click="zhuanHuan='ChangePassword'">修改密码</button>
-            <button id='mn' @click="zhuanHuan='ChangeNickname'">修改昵称</button>
-    
+        <div id='tag'>
+            <button id='pi' @click="conversion='PersonalInformation'">个人信息</button>
+            <button id='mp' @click="conversion='ChangePassword'">修改密码</button>
+            <button id='mn' @click="conversion='ChangeNickname'">修改昵称</button>
         </div>
-        <component :is='zhuanHuan'></component>
+        <component :is='conversion'></component>
     </div>
 </template>
 <script>
@@ -21,58 +20,53 @@ export default {
     },
     data: function () {
         return {
-            zhuanHuan: 'PersonalInformation'
+            conversion: 'PersonalInformation'
         }
     }
 }
 </script>
 <style scoped>
-    #title1 {
-        height: 300px;
-        width: 700px;
-        border: 5px solid black;
-    }
+#title1 {
+    height: 300px;
+    width: 700px;
+    border: 5px solid black;
+}
 
-    #biaoqian {
-        position: absolute;
-        top: 40px;
-        padding-left: 20px;
-        display: flex;
-        flex-direction: column;
-        width: 200px;
-        height: 120px;
-    }
+#tag {
+    position: absolute;
+    top: 40px;
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    height: 120px;
+}
+#pi {
+    flex: 1 1 40px;
+    border: 1px solid grey;
+    text-align: center;
+    line-height: 40px;
+}
 
-    #pi {
-        flex: 1 1 40px;
-        border: 1px solid grey;
-        text-align: center;
-        line-height: 40px;
-    }
+#mp {
+    flex: 1 1 40px;
+    border: 1px solid grey;
+    text-align: center;
+    line-height: 40px;
+}
 
-    #mp {
-        flex: 1 1 40px;
-        border: 1px solid grey;
-        text-align: center;
-        line-height: 40px;
-    }
+#mn {
+    flex: 1 1 40px;
+    border: 1px solid grey;
+    text-align: center;
+    line-height: 40px;
+}
 
-    #mn {
-        flex: 1 1 40px;
-        border: 1px solid grey;
-        text-align: center;
-        line-height: 40px;
-    }
-
-
-
-    #centerdiv {
-        float: left;
-        width: 250px;
-        border-right: 1px dashed black;
-        padding-bottom: 300px;
-        /*关键*/
-        margin-bottom: -300px;
-        /*关键*/
-    }
+#centerdiv {
+    float: left;
+    width: 250px;
+    border-right: 1px dashed black;
+    padding-bottom: 300px;
+    margin-bottom: -300px;
+}
 </style>
