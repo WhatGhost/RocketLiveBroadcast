5d8b98fe07a35d6434cbc836bb960377c20d3389
tsy <779107309@qq.com>
2017-08-03 23:51:48 +0800

    Add personal information pages to project.Ref #23

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,11 +1,78 @@
 <template>
-    <div>
-        info
+    <div id='title1'>
+        <div id='biaoqian'>
+            <button id='pi' @click="zhuanHuan='PersonalInformation'">个人信息</button>
+            <button id='mp' @click="zhuanHuan='ChangePassword'">修改密码</button>
+            <button id='mn' @click="zhuanHuan='ChangeNickname'">修改昵称</button>
+    
+        </div>
+        <component :is='zhuanHuan'></component>
     </div>
 </template>
-<style scoped>
-div {
-    height: 500px;
-    border: 5px solid black;    
+<script>
+import PersonalInformation from './PersonalInformation.vue'
+import ChangePassword from './ChangePassword.vue'
+import ChangeNickname from './ChangeNickname.vue'
+export default {
+    components: {
+        PersonalInformation,
+        ChangeNickname,
+        ChangePassword,
+    },
+    data: function () {
+        return {
+            zhuanHuan: 'PersonalInformation'
+        }
+    }
 }
+</script>
+<style scoped>
+    #title1 {
+        height: 300px;
+        width: 700px;
+        border: 5px solid black;
+    }
+
+    #biaoqian {
+        position: absolute;
+        top: 40px;
+        padding-left: 20px;
+        display: flex;
+        flex-direction: column;
+        width: 200px;
+        height: 120px;
+    }
+
+    #pi {
+        flex: 1 1 40px;
+        border: 1px solid grey;
+        text-align: center;
+        line-height: 40px;
+    }
+
+    #mp {
+        flex: 1 1 40px;
+        border: 1px solid grey;
+        text-align: center;
+        line-height: 40px;
+    }
+
+    #mn {
+        flex: 1 1 40px;
+        border: 1px solid grey;
+        text-align: center;
+        line-height: 40px;
+    }
+
+
+
+    #centerdiv {
+        float: left;
+        width: 250px;
+        border-right: 1px dashed black;
+        padding-bottom: 300px;
+        /*关键*/
+        margin-bottom: -300px;
+        /*关键*/
+    }
 </style>
