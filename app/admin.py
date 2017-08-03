0724b6552f6a6d8c6f4326fc738c0ea14ba47849
lijin <605944389@qq.com>
2017-08-03 17:29:53 +0800

    Add models. Ref #15

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Room,LiveRoom,VideoRoom,AllSilent,OneSilent,ChatHistory,VisitHistory
+
+admin.site.register(Room)
+admin.site.register(LiveRoom)
+admin.site.register(VideoRoom)
+admin.site.register(AllSilent)
+admin.site.register(OneSilent)
+admin.site.register(ChatHistory)
+admin.site.register(VisitHistory)
 # Register your models here.
