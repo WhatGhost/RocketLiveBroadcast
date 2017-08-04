ada762d4f2451015610acef3fc6dfe3baf5aadf4
tsy <779107309@qq.com>
2017-08-04 16:21:18 +0800

    Add change nickname page to project. Ref #27

--- src/components/ChangeNickname.vue
+++ src/components/ChangeNickname.vue
@@ -1,39 +1,46 @@
 <template>
     <div>
-    <div id='title2'>用户昵称</div>
-    <input name="key" type="text" id="key" value="Leto" size="30"   
-          onmouseover=this.focus();
-          this.select();   
-          onclick="if(value==defaultValue){value='';this.style.color='#000'}"   
-          onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999"/>
-    <div><button id='change' onclick=alert("修改成功")>确认修改</button></div>  
+        <div id='title2'>用户昵称</div>
+        <input name="key" type="text" id="key" value="Leto" size="30" onmouseover=this.focus(); this.select(); />
+        <div>
+            <button id='change' onclick=window.alert("修改成功")>确认修改</button>
+        </div>
     </div>
 </template>
 <style scoped>
-#title2{
-    padding-top:100px;
+#title2 {
+    padding-top: 100px;
 }
-#key{
-    position:absolute;
-    width:250px;
-    height:30px;
-    margin-left:320px;
+
+#key {
+    position: absolute;
+    width: 250px;
+    height: 30px;
+    margin-left: 320px;
 }
-#change{
-    position:absolute;
-    width:100px;
-    height:30px;
-    margin-top:50px;
-    margin-left:150px;
+
+#change {
+    position: absolute;
+    width: 100px;
+    height: 30px;
+    margin-top: 50px;
+    margin-left: 150px;
     color: white;
     background-color: green;
     border-radius: 10px;
-    background: #a5cd4e; /* Old browsers */
-    background: -moz-linear-gradient(top,  #a5cd4e 0%, #6b8f1a 100%); /* FF3.6+ */
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#a5cd4e), color-stop(100%,#6b8f1a)); /* Chrome,Safari4+ */
-    background: -webkit-linear-gradient(top,  #a5cd4e 0%,#6b8f1a 100%); /* Chrome10+,Safari5.1+ */
-    background: -o-linear-gradient(top,  #a5cd4e 0%,#6b8f1a 100%); /* Opera 11.10+ */
-    background: -ms-linear-gradient(top,  #a5cd4e 0%,#6b8f1a 100%); /* IE10+ */
-    background: linear-gradient(top,  #a5cd4e 0%,#6b8f1a 100%); /* W3C */	
+    background: #a5cd4e;
+    /* Old browsers */
+    background: -moz-linear-gradient(top, #a5cd4e 0%, #6b8f1a 100%);
+    /* FF3.6+ */
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #a5cd4e), color-stop(100%, #6b8f1a));
+    /* Chrome,Safari4+ */
+    background: -webkit-linear-gradient(top, #a5cd4e 0%, #6b8f1a 100%);
+    /* Chrome10+,Safari5.1+ */
+    background: -o-linear-gradient(top, #a5cd4e 0%, #6b8f1a 100%);
+    /* Opera 11.10+ */
+    background: -ms-linear-gradient(top, #a5cd4e 0%, #6b8f1a 100%);
+    /* IE10+ */
+    background: linear-gradient(top, #a5cd4e 0%, #6b8f1a 100%);
+    /* W3C */
 }
 </style>
