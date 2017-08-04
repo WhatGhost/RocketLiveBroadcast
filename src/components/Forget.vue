ce90a75eaa907d71a9889ebfa930a1af1022f0c4
leto <1907984744@qq.com>
2017-08-05 01:24:40 +0800

    Add forget password page and fix its bugs.Ref #36

--- src/components/Forget.vue
+++ src/components/Forget.vue
@@ -1,44 +1,42 @@
-<<template>
+<template>
      <div class="main">
         <h1>找回密码</h1>
         <input type="text" placeholder="账号邮箱" v-model="account">
-        <input type="text" placeholder="验证码" v-model="vertificate-code">
-        <button @click="send-vertificate-code">发送验证码</button>
+        <div class="vertificate">
+            <input type="text" placeholder="验证码" v-model="vertificateCode" class="vertificate-input">
+            <button @click="send-vertificate-code" class="send-vertificate-code-btn">发送验证码</button>
+        </div>
         <input type="password" placeholder="密码" v-model="password">
-        <input type="password" placeholder="确认密码" v-model="sure-password">
-        <button @click="checkVertification">确认</button>
+        <input type="password" placeholder="确认密码" v-model="surePassword">
+        <button @click="checkVertification" class="sure-btn">确认</button>
     </div>
 </template>
 
-<<script>
+<script>
 export default {
-  data: function () {
-      return {
-          account: '',
-          vertificateCode: '',
-          password: '',
-          surePassword: ''
-      },
-      methods: {
-          sendVertificateCode() {
+    data: function () {
+        return {
+            account: '',
+            vertificateCode: '',
+            password: '',
+            surePassword: '',
+        }
+    },
+    methods: {
+        sendVertificateCode() {
 
-          },
-          checkVertification() {
-              //check vertification code
-              if(right) {
-                  setPassword()
-              }
-          },
-          setPassword() {
-              
-          }
-      }
-  }
+        },
+        checkVertification() {
+            // check vertification code
+            if (true) {
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
-.main {
+.main > * {
     margin-left: auto;
     margin-right: auto;
 }
@@ -66,7 +64,7 @@ input {
 
 .vertificate-input {
     display: inline-block;
-    width: 150px;
+    width: 250px;
 }
 
 .send-vertificate-code-btn {
@@ -76,6 +74,7 @@ input {
     height: 3em;
     border: none;
     border-radius: 10px;
+    margin-right: auto;
 }
 
 .sure-btn {
@@ -87,7 +86,7 @@ input {
     border-radius: 10px;
     width: 350px;
     height: 40px;
-    margin: 15px;
+    margin-top: 15px;
 }
 
 input::-webkit-input-placeholder {
