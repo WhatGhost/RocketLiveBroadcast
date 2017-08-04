ec7f96206e731c2795a34ffc414a03c48136a151
lijin <605944389@qq.com>
2017-08-04 11:29:16 +0800

    Edit models extend the AbstractBaseUser,BaseUserManage,create a new useradmin and debug. Ref #15

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,85 @@
 from django.contrib import admin
-
 from .models import Room,LiveRoom,VideoRoom,AllSilent,OneSilent,ChatHistory,VisitHistory
+from django import forms 
+from django.contrib.auth.models import Group 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField 
+from .models import MyUser 
 
+
+class UserCreationForm(forms.ModelForm): 
+    """A form for creating new users.
+    Includes all the required
+    fields, plus a repeated password.""" 
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput) 
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput) 
+    class Meta: 
+        model = MyUser 
+        fields = ('account', 'nickname','is_student') 
+    def clean_password2(self): 
+        # Check that the two password entries match 
+        password1 = self.cleaned_data.get("password1") 
+        password2 = self.cleaned_data.get("password2") 
+        if password1 and password2 and password1 != password2: 
+            raise forms.ValidationError("Passwords don't match") 
+        return password2 
+    def save(self, commit=True): 
+        # Save the provided password in hashed format 
+        user = super(UserCreationForm, self).save(commit=False) 
+        user.set_password(self.cleaned_data["password1"]) 
+        if commit: 
+            user.save() 
+        return user
+
+
+class UserChangeForm(forms.ModelForm): 
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """ 
+    password = ReadOnlyPasswordHashField() 
+    class Meta: 
+        model = MyUser 
+        fields = ('account', 'password', 'nickname','is_student', 'is_active', 'is_admin') 
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value. 
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value 
+            return self.initial["password"]
+
+
+class MyUserAdmin(UserAdmin): 
+    # The forms to add and change user instances 
+    form = UserChangeForm 
+    add_form = UserCreationForm 
+    # The fields to be used in displaying the User model. 
+    # These override the definitions on the base UserAdmin 
+    # that reference specific fields on auth.User. 
+    list_display = ('account', 'nickname', 'is_student','is_admin') 
+    list_filter = ('is_admin',) 
+    fieldsets = ( 
+        (None, {'fields': ('account', 'password')}), 
+        ('Personal info', {'fields': ('nickname','is_student')}), 
+        ('Permissions', {'fields': ('is_admin',)}), 
+    ) 
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin 
+    # overrides get_fieldsets to use this attribute when creating a user. 
+    add_fieldsets = ( 
+        (None, { 
+            'classes': ('wide',),
+            'fields': ('account', 'nickname','is_student', 'password1', 'password2')} 
+        ), 
+    ) 
+    search_fields = ('account',) 
+    ordering = ('account',) 
+    filter_horizontal = () 
+
+
+# Now register the new UserAdmin... 
+admin.site.register(MyUser, MyUserAdmin) 
+# ... and, since we're not using Django's built-in permissions, 
+# unregister the Group model from admin. 
+admin.site.unregister(Group)
 admin.site.register(Room)
 admin.site.register(LiveRoom)
 admin.site.register(VideoRoom)
--- app/models.py
+++ app/models.py
@@ -1,12 +1,70 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser ) 
+class MyUserManager(BaseUserManager): 
+    def create_user(self,account, nickname,is_student=True,password=None): 
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        #if not email: 
+         #    raise ValueError('Users must have an email address') 
+        user = self.model( account=account,nickname=nickname,is_student=is_student)
+        user.set_password(password) 
+        user.save(using=self._db) 
+        return user 
+    def create_superuser(self, account,nickname, is_student,password): 
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """ 
+        user = self.create_user(account=account,nickname=nickname, password=password, is_student=is_student ) 
+        user.is_admin = True 
+        user.save(using=self._db) 
+        return user 
 
+
+class MyUser(AbstractBaseUser): 
+    account=models.CharField(max_length=250,unique=True) 
+    nickname=models.CharField(max_length=200)
+    is_student=models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True) 
+    is_admin = models.BooleanField(default=False) 
+    objects = MyUserManager() 
+    USERNAME_FIELD = 'account' 
+    #设置认证标识
+    REQUIRED_FIELDS = ['nickname','is_student']
+    def get_full_name(self): 
+        # The user is identified by their email address 
+        return self.account
+    def get_short_name(self): 
+        # The user is identified by their email address 
+        return self.account
+    def __str__(self): 
+        # __unicode__ on Python 2 
+        return self.account
+    def has_perm(self, perm, obj=None): 
+        "Does the user have a specific permission?" 
+        # Simplest possible answer: Yes, always 
+        return True 
+    def has_module_perms(self, app_label): 
+        "Does the user have permissions to view the app `app_label`?" 
+        # Simplest possible answer: Yes, always 
+        return True     
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?" 
+    # Simplest possible answer: All admins are staff 
+        return self.is_admin
 # Create your models here.
+
+
 class Room(models.Model):
     text = models.CharField(max_length=200)
     def __str__(self):
         return 'Room:' + self.text
+
+
 class LiveRoom(models.Model):
     room_id=models.IntegerField(primary_key=True)
     room_name=models.CharField(max_length=100)
@@ -14,6 +72,8 @@ class LiveRoom(models.Model):
     room_img=models.ImageField(upload_to='liveroomimg')
     room_creater=models.CharField(max_length=100)
     created_time=models.DateTimeField()
+
+
 class VideoRoom(models.Model):
     room_id=models.IntegerField(primary_key=True)
     room_name=models.CharField(max_length=100)
@@ -23,23 +83,32 @@ class VideoRoom(models.Model):
     created_time=models.DateTimeField()
     videofile=models.FileField()
     time_length=models.IntegerField()
+
+
 class AllSilent(models.Model):
     room_id=models.IntegerField()
     is_allsilent=models.BooleanField()
     silent_time=models.DateTimeField()
+
+
 class OneSilent(models.Model):
     room_id=models.IntegerField()
     user=models.CharField(max_length=100)
     is_silent=models.BooleanField()
     silent_time=models.DateTimeField()
+
+
 class ChatHistory(models.Model):
     room_id=models.IntegerField()
     user=models.CharField(max_length=100)
     content=models.TextField()
     time=models.DateTimeField()
+
+
 class VisitHistory(models.Model):
     room_id=models.IntegerField()
     user=models.CharField(max_length=100)
     visit_time=models.DateTimeField()
     leave_time=models.DateTimeField()
     is_refused=models.BooleanField()
+
