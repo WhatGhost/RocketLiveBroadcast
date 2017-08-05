f6ddd8484a0023793e41914679718642a3416348
zhang wenda <595757067@qq.com>
2017-08-06 00:54:55 +0800

    Fix python code style problem, and correct the usage of rest framework router in urls.py. Ref #54

--- app/serializers.py
+++ app/serializers.py
@@ -1,11 +1,15 @@
-from .models import Room,MyUser
+from .models import Room, MyUser
 from rest_framework import serializers
+
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('text',)
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
-        fields = ('account', 'nickname', 'is_student', 'is_active', 'is_admin', 'password')
+        fields = ('account', 'nickname', 'is_student',
+                  'is_active', 'is_admin', 'password')
--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib import auth
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
-from .models import Room,MyUser,MyUserManager
-from .serializers import RoomSerializer,UserSerializer
-# Create your views here.
+from .models import Room, MyUser, MyUserManager
+from .serializers import RoomSerializer, UserSerializer
+
+
 def index(request):
     return render(request, 'index.html')
 
@@ -15,29 +16,29 @@ class RoomViewSet(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
 
     @list_route(methods=['delete'])
-    def clear_rooms(self,request):
+    def clear_rooms(self, request):
         rooms = Room.objects.all()
         rooms.delete()
         return HttpResponse(status=200)
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset=MyUser.objects.all()
-    serializer_class=UserSerializer
-    
-    def post(self,request):
-        MyUserManager.create_user(request.account,request.nickname,request.is_student,request.password)
+    queryset = MyUser.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        MyUserManager.create_user(
+            request.account, request.nickname, request.is_student, request.password)
         return HttpResponse(status=200)
     #@list_route(methods=['post'])
-    #def register_user(self,request):
+    # def register_user(self,request):
         #MyUser.create_user(request.useremail, request.usernickname, True,request.password)
-        #return HttpResponse(status=200)
-    def get(self,request):
-        account=request.account
-        user=get_object_or_404(queryset,account=account)
-        password=user.password
-        if user.password==request.password:
-            auth.login(request,user)
+        # return HttpResponse(status=200)
+
+    def get(self, request):
+        account = request.account
+        user = get_object_or_404(queryset, account=account)
+        password = user.password
+        if user.password == request.password:
+            auth.login(request, user)
             return HttpResponse(status=200)
-
-
