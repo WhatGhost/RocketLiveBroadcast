0aad04e72377bf86530fbd17e9af994f4ee2fda0
zhang wenda <595757067@qq.com>
2017-08-06 18:27:38 +0800

    Allowing creating room with all information provided. Ref #58

--- app/views.py
+++ app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import auth
 from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.decorators import list_route
 from .models import Room, MyUser, MyUserManager, LiveRoom
 from .serializers import RoomSerializer, UserSerializer, LiveRoomSerializer
+import json
 
 
 def index(request):
@@ -25,6 +27,22 @@ class RoomViewSet(viewsets.ModelViewSet):
 class LiveRoomViewSet(viewsets.ModelViewSet):
     queryset = LiveRoom.objects.all()
     serializer_class = LiveRoomSerializer
+
+    def create(self, request):
+        new_room = self.get_serializer(data=request.data)
+        new_room.is_valid()
+        print(new_room.errors)
+        # if LiveRoom.objects.all():
+        #     new_room['room_id'] = LiveRoom.objects.all(
+        #     ).aggregate(Max("room_id"))
+        # else:
+        #     new_room['room_id'] = 1
+        
+        if new_room.is_valid():
+            new_room.save()
+            return Response(new_room.data, status=200)
+        else:
+            return HttpResponse(400)
 
 
 class UserViewSet(viewsets.ModelViewSet):
