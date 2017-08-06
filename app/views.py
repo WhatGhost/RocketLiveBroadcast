97852ec1be3bd9ed639dadffb09259ca15946a48
Gao Yuyang <Gaomeyy@gmail.com>
2017-08-06 17:33:24 +0800

    Link verification frontend and backend to click a button to send a email. Ref #60

--- app/views.py
+++ app/views.py
@@ -5,7 +5,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import list_route
 from .models import Room, MyUser, MyUserManager
 from .serializers import RoomSerializer, UserSerializer
-
+from .send_verification import sendMail
+import json
 
 def index(request):
     return render(request, 'index.html')
@@ -27,6 +28,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     def post(self, request):
+        print(request.account)
         MyUserManager.create_user(
             request.account, request.nickname, request.is_student, request.password)
         return HttpResponse(status=200)
@@ -42,3 +44,13 @@ class UserViewSet(viewsets.ModelViewSet):
         if user.password == request.password:
             auth.login(request, user)
             return HttpResponse(status=200)
+    
+    @list_route(methods = ['post'])
+    def sendVertificateCode(self ,request):
+        account = json.loads(str(request.body,encoding='utf-8'))['account']
+        print(account)
+        vertificate = sendMail(account)
+        if(vertificate != -1):
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=404)
