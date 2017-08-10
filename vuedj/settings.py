01e1136a9366c781e6c541c4691ca44d31e3fbeb
tsy <779107309@qq.com>
2017-08-11 00:36:50 +0800

    Read test code and add info test code. Ref #92

--- vuedj/settings.py
+++ vuedj/settings.py
@@ -150,6 +150,4 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=foo,bar',
 ]
