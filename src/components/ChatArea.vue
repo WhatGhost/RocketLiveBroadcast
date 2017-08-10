dd3d00f4b831ebf0341a5e5b40ef678adfdc6876
zhang wenda <595757067@qq.com>
2017-08-11 01:12:40 +0800

    Add a simple chat room in room page.At present there is only one room. Ref #75

--- src/components/ChatArea.vue
+++ src/components/ChatArea.vue
@@ -1,20 +1,63 @@
 <template>
     <div class="main-div">
-        <h1>Chat Area</h1>
+        <ul>
+            <li v-for="msg in messageList">
+                {{ msg.nickname}}: {{ msg.content }}
+            </li>
+        </ul>
+        <button @click='sendMessage'>send</button>
+        <input id="m" v-model="message" autocomplete="off">
     </div>
 </template>
 
 <script>
-    export default {
-        data: function () {
-            return {}
+import Chat from './Chat'
+import io from '../../lib/socket.io'
+
+export default {
+    components: {
+        Chat
+    },
+    data: function () {
+        return {
+            messageList: [],
+            message: '',
+            nickname: 'xiaoming',
+            httpServer: null
+        }
+    },
+    mounted() {
+        this.connectEvent()
+    },
+    methods: {
+        connectEvent() {
+            this.httpServer = io.connect('http://127.0.0.1:3000')
+            this.httpServer.on('message', (obj) => {
+                this.messageList.push(obj)
+            })
         },
-        methods: function () {
+        sendMessage() {
+            if (this.message !== '') {
+                this.httpServer.emit('message', {content: this.message, nickname: this.nickname})
+            }
+            this.messageList.push({content: this.message, nickname: this.nickname})
+            this.message = ''
         }
     }
+}
 </script>
 
 <style scoped>
-    .main-div {
-    }
+/* copied from socket-io guidance */
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+li {
+    text-align: left;
+}
+
 </style>
