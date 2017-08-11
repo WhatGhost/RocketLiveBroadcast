c820e0256b37a3989b0df75dacf16c08f6c523f6
tsy <779107309@qq.com>
2017-08-11 23:32:08 +0800

    Display right info. Ref #107

--- src/components/PersonalInformation.vue
+++ src/components/PersonalInformation.vue
@@ -1,24 +1,39 @@
 <template>
     <div id="content">
         <div class="line">
-            <label id="nickname-label">用户昵称　</label>
-            <label id="nickname-value">leto</label>
+            <label id="nickname-label">用户昵称</label>
+            <label id="nickname-value">{{userName}}</label>
         </div>
         <div class="line">
-            <label id="e-mail-label">用户邮箱　</label>
-            <label id="e-mail-value">123456789@qq.com</label>
+            <label id="e-mail-label">用户邮箱</label>
+            <label id="e-mail-value">{{userAccount}}</label>
         </div>
     </div>
 </template>
-<style scoped>
-    #content {
-        display: flex;
-        flex-direction: column;
-        margin: 40px;
+<script>
+export default {
+    computed: {
+        userName: function () {
+            console.log(this.$store.state.nickname)
+            return this.$store.state.nickname
+        },
+        userAccount: function () {
+            console.log(this.$store.state.account)
+            return this.$store.state.account
+        }
     }
+}
+</script>
 
-    .line {
-        margin: 10px;
-        display: flex;
-    }
+<style scoped>
+#content {
+    display: flex;
+    flex-direction: column;
+    margin: 40px;
+}
+
+.line {
+    margin: 10px;
+    display: flex;
+}
 </style>
