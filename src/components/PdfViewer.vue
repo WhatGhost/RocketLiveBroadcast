cc82cd0390fc54e6995cde11847d809a93c77b12
zhang wenda <595757067@qq.com>
2017-08-12 17:10:30 +0800

    Add pgup and pgdn function. Ref #125

--- src/components/PdfViewer.vue
+++ src/components/PdfViewer.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="main-div" :class="{ hiding: hide }">
         <h1>PDFViewer</h1>
-        <pdf src="../static/maplestory.pdf"></pdf>
+        <button @click="pgup">Previous Page</button>
+        <button @click="pgdn">Next Page</button>
+        <input v-model.number="page" type="number" style="width: 5em"> /{{numPages}}
+        <pdf src="../static/maplestory.pdf" :page=page ref="pdf" @numPages="numPages=$event"></pdf>
     </div>
 </template>
 
@@ -15,8 +18,19 @@
             pdf: pdf
         },
         data: function () {
-            return {}
+            return {
+                page: 1,
+                numpages: 0
+            }
         },
+        methods: {
+            pgup() {
+                this.page = this.page - 1
+            },
+            pgdn() {
+                this.page = this.page + 1
+            }
+        }
     }
 </script>
 
