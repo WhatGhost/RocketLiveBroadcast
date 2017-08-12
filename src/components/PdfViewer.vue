0b441fc8f3e94801e20b855baa250339108a05f4
zhang wenda <595757067@qq.com>
2017-08-12 16:20:36 +0800

    Display pdf on left side. Ref #122

---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main-div" :class="{ hiding: hide }">
+        <h1>PDFViewer</h1>
+        <pdf src="../static/maplestory.pdf"></pdf>
+    </div>
+</template>
+
+<script>
+    import pdf from 'vue-pdf'
+    export default {
+        props: {
+            hide: true,
+        },
+        components: {
+            pdf: pdf
+        },
+        data: function () {
+            return {}
+        },
+    }
+</script>
+
+<style scoped>
+    .hiding {
+        display: none;
+    }
+</style>
