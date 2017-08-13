e1c83b708910ce7b99a34160aea7be3ab0a64b97
Gao Yuyang <Gaomeyy@gmail.com>
2017-08-13 13:56:38 +0800

    Build frontend test framework. Ref #74

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import Hello from 'src/components/Hello'
+import Hello from '@/components/Hello'
+// import ElementUI from 'element-ui';
+
+// Vue.use(ElementUI);
 
 describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const vm = new Vue({
-      template: '<div><hello></hello></div>',
-      components: { Hello }
-    }).$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent).to.contain('Hello World!')
-  })
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(Hello)
+        const vm = new Constructor().$mount()
+        expect(vm.$el.querySelector('.hello h1').textContent)
+            .to.equal('Welcome to Your Vue.js App')
+    })
 })
