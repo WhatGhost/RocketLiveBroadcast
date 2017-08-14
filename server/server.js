9391413c1bc431480d972b31a274b30a1a9b2b64
zhang wenda <595757067@qq.com>
2017-08-14 14:24:22 +0800

    Sync pdf and code when a student enters the room. Ref #140

--- server/server.js
+++ server/server.js
@@ -2,20 +2,34 @@ var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+let ROOMDATA = {}
+
 io.on('connection', function (socket) {
     console.log('新连接已创建 !')
     socket.on('init', function(obj) {
         console.log('enter' + obj.roomId)
         socket.join(obj.roomId)
+        if (!ROOMDATA.hasOwnProperty(obj.roomId)) {
+            ROOMDATA[obj.roomId] = {
+                page: 1,
+                code: ''
+            }
+        }
     })
     socket.on('message', function(obj) {
         this.to(obj.roomId).emit('message', obj)
     })
     socket.on('changePage', function(obj) {
         this.to(obj.roomId).emit('changePage', obj)
+        ROOMDATA[obj.roomId].page = obj.page
     })
     socket.on('changeCode', function(obj) {
         this.to(obj.roomId).emit('changeCode', obj)
+        ROOMDATA[obj.roomId].code = obj.code
+    })
+    socket.on('getCurrentData', function(obj) {
+        io.in(socket.id).emit('changePage', ROOMDATA[obj.roomId])
+        io.in(socket.id).emit('changeCode', ROOMDATA[obj.roomId])
     })
 })
 
