cb70dea9697770e3f7fcedc6f3bc2b3ac842fc60
zhang wenda <595757067@qq.com>
2017-08-19 00:18:39 +0800

    Add whiteboard. Ref #143

--- src/components/WhiteboardPage.vue
+++ src/components/WhiteboardPage.vue
@@ -1,28 +1,80 @@
 <template>
-    <div class="main-div" :class="{ hiding: hide }">
-        <h1>WhiteBoard</h1>
+    <div :class="{ hiding: hide }">
+        <button @click="mountlc">create</button>
+        <div class="fs-container">
+            <div id="lc" ref="lcanvas"></div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            hide: true,
+export default {
+    props: {
+        hide: true,
+    },
+    // mounted() {
+    //     this.lc = window.LC.init(document.getElementById('lc'), {
+    //         imageURLPrefix: 'static/static/canvas/_assets/lc-images',
+    //         toolbarPosition: 'bottom',
+    //         defaultStrokeWidth: 2,
+    //         strokeWidths: [1, 2, 3, 5, 30],
+    //         imageSize: {
+    //             width: 500,
+    //             height: null
+    //         }
+    //     })
+    // },
+    data: function () {
+        return {
+            lc: null
+        }
+    },
+    mounted() {
+        this.lc = window.LC.init(this.$refs.lcanvas, {
+            imageURLPrefix: 'static/static/canvas/_assets/lc-images',
+            toolbarPosition: 'bottom',
+            defaultStrokeWidth: 2,
+            strokeWidths: [1, 2, 3, 5, 30],
+            imageSize: {
+                width: 500,
+                height: null
+            }
+        })
+    },
+    methods: {
+        out: function () {
+            let j = JSON.stringify(this.lc.getSnapshot())
+            console.log(j)
         },
-        data: function () {
-            return {}
-        },
-//        methods: function () {
-//        }
+        mountlc: function () {
+            this.lc = window.LC.init(document.getElementById('lc'), {
+                imageURLPrefix: 'static/static/canvas/_assets/lc-images',
+                toolbarPosition: 'bottom',
+                defaultStrokeWidth: 2,
+                strokeWidths: [1, 2, 3, 5, 30],
+                imageSize: {
+                    width: 500,
+                    height: null
+                }
+            })
+        }
     }
+}
 </script>
 
 <style scoped>
-    .main-div {
-        background-color: #00af50;
-    }
+.hiding {
+    display: none;
+}
 
-    .hiding {
-        display: none;
-    }
+.fs-container {
+    width: 320px;
+    margin: auto;
+}
+
+.literally {
+    width: 100%;
+    height: 100%;
+    position: relative;
+}
 </style>
