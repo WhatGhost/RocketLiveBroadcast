29cc8ba309a5231277ebba17e78bc037abd163ef
lijin <605944389@qq.com>
2017-08-21 21:28:46 +0800

    Add the function to change the code mode. Ref #207

--- src/components/CodeEditorPage.vue
+++ src/components/CodeEditorPage.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="main-div" :class="{ hiding: hide }">
-        <codemirror ref="codemirror" v-model="code" :options="editorOptions" @change="emitCodeChange" ></codemirror>
+        <select ref="sel" class="codeLanguage" size="1" @change="update(selectLan.options[selectLan.options.selectedIndex].value)">
+        <option value='text/x-csrc'>c</option>
+        <option value='text/x-c++src'>c++</option>
+        <option value='text/x-php'>php</option>
+        <option value='text/x-python'>python</option>
+        <option value='text/html'>html</option>
+        <option value='text/x-java'>java</option>
+        <option value='text/x-csharp'>c#</option>
+        <option value='text/x-scala'>scala</option>
+        <option value='text/javascript' selected=true>javascript</option>
+        <option value='text/x-pascal'>pascal</option>
+        <option value='text/x-ruby'>ruby</option>
+        <option value='application/xml'>xml</option>
+        <option value='text/css'>css</option>
+        </select>    
+        <button @click="changeC">change</button>
+        <codemirror ref="codemirror" v-model="code" :options="editorOptions" @change="emitCodeChange" v-if="langChanged" ></codemirror>
     </div>
 </template>
 
@@ -21,11 +37,13 @@ export default {
     },
     data() {
         return {
+            langChanged: true,
             code: '',
             syncCode: '',
+            selectLan: null,
             editorOptions: {
                 tabSize: 4,
-                mode: 'text/javascript',
+                mode: 'text/x-python',
                 theme: 'base16-light',
                 lineNumbers: true,
                 line: true,
@@ -33,6 +51,9 @@ export default {
                 readOnly: true
             }
         }
+    },
+    mounted: function() {
+        this.selectLan = this.$refs.sel
     },
     methods: {
         emitCodeChange(event) {
@@ -43,6 +64,15 @@ export default {
                 roomId: this.roomInfo.roomId,
                 code: event
             })
+        },
+        update(val) {
+            console.log(this.editorOptions['mode'])
+            console.log('hhh')
+            console.log(val)
+            this.editorOptions['mode'] = val
+        },
+        changeC() {
+            this.langChanged = !this.langChanged
         }
     }
 }
